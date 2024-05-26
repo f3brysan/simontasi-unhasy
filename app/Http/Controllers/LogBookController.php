2025e5640e05cae8b658f917cfaa1cf9a2236fb7
Febry San <97709170+f3brysan@users.noreply.github.com>
2024-05-26 14:58:33 +0700

    refactor: Updated LogBookController: modified getLogBook and getDosenLogBook methods, added decryption and encryption, and refactored approveDetilLogBookMhs method.

--- app/Http/Controllers/LogBookController.php
+++ app/Http/Controllers/LogBookController.php
@@ -29,7 +29,7 @@ class LogBookController extends Controller
                         $is_approve = $data->is_approve == '1' ? 'disabled' : '';
                         $btn = '<div class="btn-group btn-group-sm" role="group" aria-label="Small button group">
                         <button type="button" class="btn btn-primary" ' . $is_approve . '><i class="fa-solid fa-gear"></i></button>
-                        <button type="button" class="btn btn-danger text-light"><i class="fa-solid fa-trash-can"></i></button>                        
+                        <button type="button" class="btn btn-danger text-light" '.$is_approve   .'><i class="fa-solid fa-trash-can"></i></button>                        
                       </div>';
                         return $btn;
                     })
@@ -122,7 +122,7 @@ class LogBookController extends Controller
 
     public function getDosenLogBook(Request $request)
     {
-        $nip = auth()->user()->no_induk;        
+        $nip = auth()->user()->no_induk;
         $getMahasiswaLogBooks = DB::table("tr_pendaftaran as p")
             ->join("users as u", function ($join) {
                 $join->on("u.no_induk", "=", "p.no_induk");
@@ -139,8 +139,8 @@ class LogBookController extends Controller
             ->where("nip", $nip)
             ->where('is_approve', '0')
             ->groupBy("pendaftaran_id")
-            ->get();            
-        
+            ->get();
+
         foreach ($getMahasiswaLogBooks as $logBook) {
             $prodi = (new GetDataAPISiakad)->getDataProdi($logBook->prodi_kode);
             $mahasiswaLogBooks[$logBook->id] = [
@@ -154,19 +154,19 @@ class LogBookController extends Controller
         foreach ($countLogBooks as $count) {
             $mahasiswaLogBooks[$count->pendaftaran_id]['total_logbook'] = $count->count;
         }
-        
-        if ($request->ajax()) {            
+
+        if ($request->ajax()) {
             return DataTables::of($mahasiswaLogBooks)
                 ->addColumn('action', function ($data) {
-                    $btn = '<a href="'.URL::to('dosen/log-bimbingan/detil/'.Crypt::encrypt($data['id'])).'" target="_blank" class="btn btn-primary btn-sm">Lihat</a>';
+                    $btn = '<a href="' . URL::to('dosen/log-bimbingan/detil/' . Crypt::encrypt($data['id'])) . '" target="_blank" class="btn btn-primary btn-sm">Lihat</a>';
                     return $btn;
-                })      
+                })
                 ->addColumn('title', function ($data) {
                     return $data['title'];
-                })  
+                })
                 ->addColumn('total_logbook', function ($data) {
-                    return '<span class="badge bg-warning text-dark">'.$data['total_logbook'].' Menunggu Approval</span>';
-                })  
+                    return '<span class="badge bg-warning text-dark">' . $data['total_logbook'] . ' Menunggu Approval</span>';
+                })
                 // Make the columns raw so that HTML can be rendered
                 ->rawColumns(['action', 'title', 'total_logbook'])
                 // Add an index column
@@ -180,14 +180,25 @@ class LogBookController extends Controller
 
     public function getDetilLogBookMhs($id, Request $request)
     {
-        $id = Crypt::decrypt($id);        
-        $logBooks = DB::table("tr_logbook")        
-        ->where("pendaftaran_id", $id)                
-        ->get();   
+        // Decrypt the ID
+        $id = Crypt::decrypt($id);
+
+        // Retrieve the logbooks for the given pendaftaran ID
+        $logBooks = DB::table("tr_logbook")
+            ->where("pendaftaran_id", $id)
+            ->get();
+
+        // Retrieve the proposal data for the given pendaftaran ID
         $dataProposal = DB::table('tr_pendaftaran as p')
             ->where('id', $id)->first();
+
+        // Retrieve the user data (mahasiswa) for the given proposal data
         $dataMHS = User::where('no_induk', $dataProposal->no_induk)->first();
+
+        // Retrieve the program study data for the given user data
         $prodi = (new GetDataAPISiakad)->getDataProdi($dataMHS->prodi_kode);
+
+        // Prepare the data for the view
         $data = [
             'dataProposal' => $dataProposal,
             'dataMHS' => $dataMHS,
@@ -195,25 +206,41 @@ class LogBookController extends Controller
             'prodi' => $prodi
         ];
 
-         // Check if the request is an AJAX request
-         if ($request->ajax()) {
+        /**
+         * Check if the request is an AJAX request
+         * Use Datatables to display the data         
+         */
+        if ($request->ajax()) {
             // Use Datatables to display the data
             return DataTables::of($logBooks)
-                ->addColumn('action', function ($data) {   
-                    $btnType = $data->is_approve == 0 ? 'btn-success' : 'btn-warning';               
-                    $btnIcon = $data->is_approve == 0 ? 'fa-check' : 'fa-arrows-rotate';               
+                /**
+                 * Add a column for the action button                 
+                 */
+                ->addColumn('action', function ($data) {
+                    // Determine the button type and icon based on the is_approve value
+                    $btnType = $data->is_approve == 0 ? 'btn-success' : 'btn-warning';
+                    $btnIcon = $data->is_approve == 0 ? 'fa-check' : 'fa-arrows-rotate';
+                    // Generate the HTML for the button
                     $btn = '<div class="btn-group btn-group-sm" role="group" aria-label="Small button group">
-                    <button type="button" data-id="'.Crypt::encrypt($data->id).'" data-status="'.$data->is_approve.'" class="approve btn '.$btnType.' text-light"><i class="fa-solid '.$btnIcon.'"></i></button>                   
+                    <button type="button" data-id="' . Crypt::encrypt($data->id) . '" data-status="' . $data->is_approve . '" class="approve btn ' . $btnType . ' text-light"><i class="fa-solid ' . $btnIcon . '"></i></button>                   
                   </div>';
-                    return $btn;                    
+                    // Return the button HTML
+                    return $btn;
                 })
-                // Add a column for the user's catatan
+                /**
+                 * Add a column for the user's catatan                 
+                 */
                 ->addColumn('catatan', function ($data) {
                     // Return the catatan value
                     return $data->catatan;
                 })
+                /**
+                 * Add a column for the status                 
+                 */
                 ->addColumn('status', function ($data) {
+                    // Determine the status badge based on the is_approve value
                     $status = $data->is_approve == '1' ? '<span class="badge bg-success">Disetujui</span>' : '<span class="badge bg-warning">Belum disetujui</span>';
+                    // Return the status badge HTML
                     return $status;
                 })
                 // Make the columns raw so that HTML can be rendered
@@ -223,21 +250,39 @@ class LogBookController extends Controller
                 // Return the Datatables object
                 ->make(true);
         }
-        
+
+        // Pass the data to the view
         return view('logbook.dosen.detil-mhs', $data);
     }
 
-    public function approveDetilLogBookMhs($id) {
+    public function approveDetilLogBookMhs($id)
+    {
+        /**
+         * Approve or reject a logbook entry
+         *
+         * @param string $id The encrypted ID of the logbook entry
+         * @return \Illuminate\Http\JsonResponse The status of the update
+         */
         try {
+            // Decrypt the ID parameter
             $id = Crypt::decrypt($id);
+
+            // Retrieve the logbook entry
             $get = DB::table('tr_logbook')->where('id', $id)->first();
+
+            // Determine the new status based on the current status
             $status = $get->is_approve == 1 ? 0 : 1;
+
+            // Update the logbook entry with the new status and current timestamp
             $update = DB::table('tr_logbook')->where('id', $id)->update([
                 'is_approve' => $status,
                 'approve_at' => date('Y-m-d H:i:s')
             ]);
+
+            // Return the new status as a JSON response
             return response()->json($status);
         } catch (\Exception $e) {
+            // If an exception occurs, return the error message as a JSON response
             return response()->json($e->getMessage());
         }
     }
